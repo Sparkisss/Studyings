fa68a12ed8aa6e457f64fd1f458ebd6b58a8f459
Igor Siamykin <sparkisisss@gmail.com>
2023-07-15 16:26:00 +0300

    upgrade

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,21 @@
 window.onload = init;
 function init() {
-  let image = document.getElementById("zero");
-  image.onclick = showAnswer;
+  let images = document.getElementsByTagName("img");
+  for(let i = 0; i < images.length; i++) {
+    images[i].onmouseover = showAnswer;
+    images[i].onmouseout = reblur;
+  }
 }
-function showAnswer() {
-  let image = document.getElementById("zero");
-  image.src = "../EventHandler/img/2.jpg";
+function showAnswer(eventObj) {
+  let image = eventObj.target;
+  let name = image.id;
+  name = "../EventHandler/img/" + name + ".jpg";
+  image.src = name;
+  //setTimeout(reblur, 2000, image);
+}
+function reblur(eventObj) {
+  let image = eventObj.target;
+  let name = image.id;
+  name = "../EventHandler/img/" + name + "blur.jpg";
+  image.src = name;
 }
